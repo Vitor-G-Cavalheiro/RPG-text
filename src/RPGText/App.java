0ac84e4c56d71338693691de1dea3c4087767726
Vitor Cavalheiro <126813410+Vitor-G-Cavalheiro@users.noreply.github.com>
2024-07-01 12:33:44 -0300

    TRUE Update 0.13

---
+++ src/RPGText/App.java
@@ -0,0 +1,8 @@
+package RPGText;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Listener listen = new Listener();
+
+       listen.startGame();
+    }
+}
---
+++ src/RPGText/entity/Dragon.java
@@ -0,0 +1,6 @@
+package RPGText.entity;
+public class Dragon extends Entity {
+    public Dragon(int strength, int agility, int inteligence, int constitution, int damage, String name){
+        super(strength, agility, inteligence, constitution, damage, name);
+    }
+}
